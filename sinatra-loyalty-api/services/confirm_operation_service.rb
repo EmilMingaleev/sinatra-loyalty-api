c8730602b191450762539314fdc0b63f045cb71b
EmilMingaleev <mingaleev_emil@mail.ru>
2025-02-28 13:44:31 +0300

    submit fix

--- sinatra-loyalty-api/services/confirm_operation_service.rb
+++ sinatra-loyalty-api/services/confirm_operation_service.rb
@@ -4,24 +4,23 @@ class ConfirmOperationService
     return operation unless operation.is_a?(Operation)
 
     write_off_data = WriteOffCalculationService.calculate(operation, write_off)
-    new_total = write_off_data[:new_total]
-    new_cashback = write_off_data[:new_cashback]
-    write_off = write_off_data[:write_off]
+    new_total = write_off_data[:new_total].round(2)
+    new_cashback = write_off_data[:new_cashback].round(2)
+    write_off = write_off_data[:write_off].round(2)
 
     OperationUpdateService.update(operation, write_off, new_total, new_cashback)
 
     {
-      status: "success",
-      message: "Operation confirmed",
+      status: 200,
+      message: "Данные успешно обработаны!",
       operation: {
         user_id: user.id,
         cashback: new_cashback,
-        cashback_earned: new_cashback,
-        cashback_percent: operation.cashback_percent.to_f,
-        total_discount: operation.discount.to_f,
-        discount_percent: operation.discount_percent.to_f,
-        written_off: write_off,
-        total_sum: new_total
+        cashback_percent: operation.cashback_percent.to_f.round(2),
+        discount: operation.discount.to_f.round(2).to_s,
+        discount_percent: operation.discount_percent.to_f.round(2).to_s,
+        write_off: write_off,
+        check_summ: new_total
       }
     }
   end
--- sinatra-loyalty-api/services/operation_validation_service.rb
+++ sinatra-loyalty-api/services/operation_validation_service.rb
@@ -1,8 +1,8 @@
 class OperationValidationService
     def self.validate(operation_id)
         operation = Operation[operation_id]
-        return { status: "error", message: "Operation not found" } unless operation
-        return { status: "error", message: "Operation already confirmed" } if operation.done
+        return { status: "error", message: "Операция не найдена!" } unless operation
+        return { status: "error", message: "Операция уже проведена!" } if operation.done
         operation
     end
 end
