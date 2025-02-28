0a9e5d606345e197f52575dccdce4d5ad4e0f5ed
Emil Mingaleev <77433697+EmilMingaleev@users.noreply.github.com>
2025-02-28 07:52:53 +0300

    big change

--- sinatra-loyalty-api/services/loyalty_service.rb
+++ sinatra-loyalty-api/services/loyalty_service.rb
@@ -12,13 +12,14 @@ class LoyaltyService
 
       unless product
         discount_details << {
-          product_id: pos[:id],
-          discount: 0,
-          cashback: 0,
-          type: "unknown",
-          description: "Product not found in the database",
-          discount_percent: 0,
-          cashback_percent: 0
+          id: pos[:id],
+          price: pos[:price],
+          quantity: pos[:quantity],
+          type: nil,
+          value: nil,
+          type_desc: nil,
+          discount_percent: 0.0,
+          discount_summ: 0.0
         }
         next
       end
@@ -30,17 +31,35 @@ class LoyaltyService
       total_sum += product_calculation[:subtotal] - product_calculation[:discount]
       total_available_write_off += product_calculation[:subtotal] - product_calculation[:discount] unless product.type == 'noloyalty'
 
-      discount_details << { 
-        product_id: pos[:id],
-        discount: product_calculation[:discount], 
-        cashback: product_calculation[:cashback], 
+      discount_details << {
+        id: pos[:id],
+        price: pos[:price],
+        quantity: pos[:quantity],
         type: product.type,
-        description: product_calculation[:description],
-        discount_percent: product_calculation[:discount_percent],
-        cashback_percent: product_calculation[:cashback_percent]
+        value: product_calculation[:value],
+        type_desc: product_calculation[:description],
+        discount_percent: product_calculation[:discount_percent].round(2),
+        discount_summ: product_calculation[:discount].round(2)
       }
     end
 
-    LoyaltyResponseFormatter.format(user, operation_id, total_sum, total_available_write_off, total_cashback, total_discount, discount_details, template)
+    if operation_id.nil?
+      operation = Operation.create(
+        user_id: user.id,
+        check_summ: total_sum,
+        cashback: 0.0,
+        cashback_percent: 0.0,
+        discount: 0.0,
+        discount_percent: 0.0,
+        done: false
+      )
+      operation_id = operation.id
+    end
+    
+
+    discount_value = total_sum.zero? ? 0.0 : ((total_discount / total_sum) * 100).round(2)
+    cashback_value = total_sum.zero? ? 0.0 : ((total_cashback / total_sum) * 100).round(2)
+
+    LoyaltyResponseFormatter.format(user, operation_id, total_sum, total_available_write_off, total_cashback, total_discount, discount_details, template, discount_value, cashback_value)
   end
 end
